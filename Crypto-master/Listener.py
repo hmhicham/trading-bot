3977707cd5600dc745e1511a16e70ac5c6ef23d7
hmhicham <113803096+hmhicham@users.noreply.github.com>
2025-06-24 13:02:23 +0100

    Update Listener.py

--- Crypto-master/Listener.py
+++ Crypto-master/Listener.py
@@ -6,15 +6,15 @@ from message import Message
 from T_binance import *
 
 
-api_id = '18899893'
-api_hash = 'e3e894d4a1b01f483be171e33e8e9677'
-phone_number = '213549851815'
+api_id = '...'
+api_hash = 'e3...'
+phone_number = '213...'
 
 client_T = TelegramClient('session_name', api_id, api_hash)
 client_T.start(phone=phone_number)
 
 # List of channel IDs
-channel_ids = [-1002472647736] 
+channel_ids = [...] 
 
 
 
